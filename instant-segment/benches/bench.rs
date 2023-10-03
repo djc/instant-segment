9fa6de0b4774bfc63ac2863f1911eacf6403ace8
Michael Partheil <michael.partheil@gmail.com>
2023-10-03 17:13:36 +0200

    Add a second benchmark

--- instant-segment/benches/bench.rs
+++ instant-segment/benches/bench.rs
@@ -5,7 +5,7 @@ use bencher::{benchmark_group, benchmark_main, Bencher};
 use instant_segment::test_data::{crate_data_dir, segmenter};
 use instant_segment::Search;
 
-benchmark_group!(benches, short);
+benchmark_group!(benches, short, long);
 benchmark_main!(benches);
 
 fn short(bench: &mut Bencher) {
@@ -13,5 +13,16 @@ fn short(bench: &mut Bencher) {
     let mut search = Search::default();
     bench.iter(|| {
         let _ = segmenter.segment("thisisatest", &mut search);
+    });
+}
+
+fn long(bench: &mut Bencher) {
+    let segmenter = segmenter(crate_data_dir());
+    let mut search = Search::default();
+    bench.iter(|| {
+        let _ = segmenter.segment(
+            "itwasabrightcolddayinaprilandtheclockswerestrikingthirteen",
+            &mut search,
+        );
     });
 }
